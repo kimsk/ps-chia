786891d64401deb7c1ed7baddd15b85707757bbc
Karlkim Suwanmongkol <karlkim@gmail.com>
2022-09-08 16:38:05 +0700

    set chia env (mainnet, testnet, sim)

---
+++ misc/set-chia-env.ps1
@@ -0,0 +1,37 @@
+[CmdletBinding(DefaultParameterSetName = 'sim')]
+param(
+    # environment
+    [parameter(
+        mandatory,
+        ParameterSetName="mainnet"
+    )]
+    [switch] $mainnet,
+    [parameter(
+        mandatory,
+        ParameterSetName="testnet"
+    )]
+    [switch] $testnet,
+    [parameter(
+        ParameterSetName="sim"
+    )]
+    [String] $sim = "main"
+)
+
+if($mainnet) {
+    $env:CHIA_ROOT = "~/.chia/mainnet"
+    $env:CHIA_KEYS_ROOT = "~/.chia_keys"
+    chia configure -t false
+} 
+elseif($testnet) {
+    $env:CHIA_ROOT = "~/.chia/testnet10"
+    $env:CHIA_KEYS_ROOT = "~/.chia_keys_testnet10"
+    chia configure -t true
+} 
+elseif($sim) {
+    $env:CHIA_ROOT = "~/.chia/simulator/$($sim)"
+    $env:CHIA_KEYS_ROOT = "~/.chia_keys_sim_$sim"
+    chia configure -t true
+}
+
+Write-Output $env:CHIA_ROOT
+Write-Output $env:CHIA_KEYS_ROOT
