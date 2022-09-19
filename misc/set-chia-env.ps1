ef67cf514587af2bdaf8226cc2c1f015a40228c7
Karlkim Suwanmongkol <karlkim@gmail.com>
2022-09-19 12:10:24 +0700

    $sim contains simulator name

--- misc/set-chia-env.ps1
+++ misc/set-chia-env.ps1
@@ -30,6 +30,7 @@ elseif($testnet) {
 elseif($sim) {
     $env:CHIA_ROOT = "~/.chia/simulator/$($sim)"
     $env:CHIA_KEYS_ROOT = "~/.chia_keys_sim_$sim"
+    New-Variable -Name "sim" -Value $sim -Force -Scope 1
     chia configure -t true
 }
 
